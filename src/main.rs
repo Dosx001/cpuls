a5c0e68b4f0efc0291258c37abe6d019fe0061c6
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-08-02 10:54:21 -0700

    feat: add systemd logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-extern crate cpuid;
-
 fn main() {
+    let formatter = syslog::Formatter3164 {
+        facility: syslog::Facility::LOG_USER,
+        hostname: None,
+        process: "cpuls".into(),
+        pid: 0,
+    };
+    let logger = match syslog::unix(formatter) {
+        Err(e) => {
+            println!("impossible to connect to syslog: {:?}", e);
+            return;
+        }
+        Ok(logger) => logger,
+    };
+    let _ = log::set_boxed_logger(Box::new(syslog::BasicLogger::new(logger)))
+        .map(|()| log::set_max_level(log::LevelFilter::Trace));
     match cpuid::clock_frequency() {
-        Some(frequency) => println!("CPU speed: {} MHz", frequency),
-        None => println!("Couldn't get CPU speed."),
+        Some(frequency) => {
+            if 3800 < frequency {
+                log::error!("CPU frequency is too high: {}", frequency);
+            } else if 3000 < frequency {
+                log::warn!("CPU frequency is high: {}", frequency);
+            } else {
+                log::info!("CPU frequency: {}", frequency);
+            }
+        }
+        None => log::error!("Failed to get CPU speed"),
     };
 }
